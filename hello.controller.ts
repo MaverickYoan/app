0782c63635f19f3d906b2d7630d24190b97b1077
MaverickYoan <ensemblepourlafetelibre@gmail.com>
2025-05-15 23:07:31 +0200

    hello

--- hello.controller.ts
+++ hello.controller.ts
@@ -1,8 +1,14 @@
-import { get } from '@loopback/rest';
+import { RestApplication, RestServer } from '@loopback/rest';
 
-export class HelloController {
-    @get('/hello')
-    hello(): string {
-        return 'Hello world!';
-    }
-}
+const app = new RestApplication();
+app.handler(({ request, response }, sequence) => {
+    sequence.send(response, 'hello world');
+});
+
+(async function start() {
+    await app.start();
+
+    const server = await app.getServer(RestServer);
+    const port = await server.get('rest.port');
+    console.log(`Server is running at http://127.0.0.1:${3000}`);
+})();
